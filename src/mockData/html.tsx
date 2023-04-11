4de8db9c79eaa1566a7b4ed0d1f1c8497f12b0f2
John Owodiye <94096184+iohanne17@users.noreply.github.com>
2023-04-11 16:50:04 +0100

    chore: code cleanup

--- src/mockData/html.tsx
+++ src/mockData/html.tsx
@@ -96,6 +96,19 @@ export const html2 = `
     <li>Therapeutic agents - Substances used to treat various health conditions, such as chemotherapy drugs for cancer treatment.</li>
     <li>Radiopharmaceuticals - Radioactive substances used for diagnostic or therapeutic purposes in nuclear medicine.</li>
     <li>Prescription drugs - Medications that can only be obtained with a prescription from a licensed healthcare provider.</li>
+    <li>Drug discovery and development - The process of identifying and developing new medications.</li>
+    <li>Clinical trials - Studies conducted to test the safety and efficacy of new medications.</li>
+    <li>FDA approval process - The regulatory process in the United States for approving new medications for sale.</li>
+    <li>Manufacturing and quality control - The processes involved in producing medications and ensuring they meet quality standards.</li>
+    <li>Packaging and labeling - The processes of packaging medications and providing information on labels about their contents and use.</li>
+    <li>Distribution and supply chain - The processes involved in getting medications from manufacturers to pharmacies and other healthcare providers.</li>
+    <li>Drug shortages - A situation in which a medication is in short supply, often due to manufacturing or other issues.</li>
+    <li>Counterfeit drugs - Medications that are fake or not produced by the manufacturer they claim to be from.</li>
+    <li>Medication errors - Mistakes that can occur during any part of the medication process, from prescribing to administering.</li>
+    <li>Drug recalls - The process of removing a medication from the market due to safety concerns or other issues.</li>
+    <li>Prescription drugs - Medications that can only be obtained with a prescription from a licensed healthcare provider.</li>
+    <li>Over-the-counter drugs - Medications that can be purchased without a prescription.</li>
+    <li>Herbal remedies - Natural supplements and plant-based medicines used to treat various health conditions.</li>
     </ul>
     <p>This section describes medicine trafficking.</p>
   </div>
